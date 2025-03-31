3c350a5a37aa5767c1ddd51b2b01d2f66764f947
Jinay2205 <jinayshah2205@gmail.com>
2025-03-31 13:48:55 +0530

    test number 2

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,8 +1,8 @@
-from src.math_operations import addition,subtraction
+from src.math_operations import add,subtraction
 
 def test_add() :
-    assert addition(2,3)==5
-    assert addition(5,7) ==12
+    assert add(2,3)==5
+    assert add(5,7) ==12
 
 def test_sub() :
     assert subtraction(4,2) == 2
